fe8fd6c53c909587c7e62b44880683b16a9148d9
HanSangWook <qazws78941@gmail.com>
2023-07-14 21:22:05 +0900

    바텀 네비게이션 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram/src/app.dart';
+import 'package:flutter_instagram/src/bindings/init_binding.dart';
+import 'package:get/get.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            elevation: 0.0,
+          )),
+      home: const App(),
+      initialBinding: InitBinding(),
+    );
+  }
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_instagram/src/widget/image_avatar.dart';
+import 'package:flutter_instagram/src/widget/image_data.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class App extends GetView<BottomNavigationController> {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => WillPopScope(
+        onWillPop: controller.popAction,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('App Bar'),
+          ),
+          body: _body(),
+          bottomNavigationBar: _bottom(),
+        ),
+      ),
+    );
+  }
+
+  Widget _bottom() {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      currentIndex: controller.pageIndex,
+      onTap: controller.changeIndex,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.black,
+      items: [
+        BottomNavigationBarItem(
+            icon: ImageData(
+              path: ImagePath.homeOff,
+            ),
+            activeIcon: ImageData(
+              path: ImagePath.homeOn,
+            ),
+            label: 'Home'),
+        BottomNavigationBarItem(
+            icon: ImageData(
+              path: ImagePath.searchOff,
+            ),
+            activeIcon: ImageData(
+              path: ImagePath.searchOn,
+            ),
+            label: 'Home'),
+        BottomNavigationBarItem(
+            icon: ImageData(
+              path: ImagePath.upload,
+            ),
+            activeIcon: ImageData(
+              path: ImagePath.upload,
+            ),
+            label: 'Home'),
+        BottomNavigationBarItem(
+            icon: ImageData(
+              path: ImagePath.reelsOff,
+            ),
+            activeIcon: ImageData(
+              path: ImagePath.reelsOn,
+            ),
+            label: 'Home'),
+        const BottomNavigationBarItem(
+            icon: ImageAvatar(
+              type: AvatarType.OFF,
+              url:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU',
+            ),
+            activeIcon: ImageAvatar(
+              type: AvatarType.ON,
+              url:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU',
+            ),
+            label: 'Home'),
+      ],
+    );
+  }
+
+  Widget _body() {
+    return IndexedStack(
+      index: controller.pageIndex,
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.black,
+        ),
+      ],
+    );
+  }
+}
