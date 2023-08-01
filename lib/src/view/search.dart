5b9999ca17227733e56323ef650c4f2e97663c04
HanSangWook <qazws78941@gmail.com>
2023-08-01 19:53:16 +0900

    커스텀 그리드 생성 완료

--- lib/src/view/search.dart
+++ lib/src/view/search.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Search extends StatelessWidget {
   const Search({super.key});
@@ -10,7 +13,7 @@ class Search extends StatelessWidget {
         body: CustomScrollView(
           slivers: [
             _appBar(),
-            // _body(),
+            _body(),
           ],
         ),
       ),
@@ -49,6 +52,65 @@ class Search extends StatelessWidget {
   }
 
   Widget _body() {
-    return Container();
+    return SliverGrid.builder(
+      gridDelegate: SliverQuiltedGridDelegate(
+        mainAxisSpacing: 1,
+        crossAxisSpacing: 1,
+        crossAxisCount: 3,
+        repeatPattern: QuiltedGridRepeatPattern.same,
+        pattern: [
+          //패턴 1
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 2
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 3
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 4
+          const QuiltedGridTile(2, 2),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 5
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 6
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 7
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          //패턴 8
+          const QuiltedGridTile(2, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+          const QuiltedGridTile(1, 1),
+        ],
+      ),
+      itemCount: 50,
+      itemBuilder: (context, index) => Container(
+        color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      ),
+    );
   }
 }
